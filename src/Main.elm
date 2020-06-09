e0481727165da7e5f4df60011d41910d1d216b43
Brendan Lane <brendan.lane07@gmail.com>
2020-06-09 08:41:58 -0400

    skeleton grid

--- src/Main.elm
+++ src/Main.elm
@@ -1,29 +1,37 @@
 module Main exposing (..)
 
-import Element exposing (Element, centerY, column, el, fill, padding, rgb255, spacing, text, width)
+import Element exposing (Element, column, el, height, padding, px, rgb255, row, spacing, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
+import List
 
 
 main =
-    Element.layout [] availabilityGrid
+    Element.layout
+        []
+        (createGrid 7 12)
 
 
-availabilityGrid =
+createGrid : Int -> Int -> Element msg
+createGrid n_cols n_rows =
+    row
+        [ spacing 1 ]
+        (List.repeat n_cols (createColumn n_rows))
+
+
+createColumn : Int -> Element msg
+createColumn n_rows =
     column
-        [ width fill, centerY, spacing 30 ]
-        [ box
-        , box
-        , box
-        ]
+        [ spacing 1 ]
+        (List.repeat n_rows createBox)
 
 
-box : Element msg
-box =
+createBox : Element msg
+createBox =
     el
-        [ Background.color (rgb255 240 0 245)
-        , Font.color (rgb255 0 0 0)
-        , padding 10
+        [ Background.color (rgb255 60 208 112)
+        , width (px 100)
+        , height (px 37)
         ]
-        (text "hello world")
+        Element.none
