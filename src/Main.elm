f709d74eb8c0cdf98ee32cd40256912445d4d68e
Brendan Lane <brendan.lane07@gmail.com>
2020-06-09 22:40:54 -0400

    update

--- src/Main.elm
+++ src/Main.elm
@@ -4,16 +4,18 @@ import Element exposing (Element, height, px, rgb255, text, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
+import List exposing (map, range)
+import String exposing (fromInt)
 
 
 main =
     Element.layout
         []
-        createTitleRow
+        createGrid
 
 
-daysOfWeek : List String
-daysOfWeek =
+days : List String
+days =
     [ "Sunday"
     , "Monday"
     , "Tuesday"
@@ -24,34 +26,81 @@ daysOfWeek =
     ]
 
 
-createTitleRow : Element msg
-createTitleRow =
+times : List String
+times =
+    [ "6 AM"
+    , "7 AM"
+    , "8 AM"
+    , "9 AM"
+    , "10 AM"
+    , "11 AM"
+    , "12 PM"
+    , "1 PM"
+    , "2 PM"
+    , "3 PM"
+    , "4 PM"
+    , "5 PM"
+    , "6 PM"
+    , "7 PM"
+    , "8 PM"
+    , "9 PM"
+    , "10 PM"
+    ]
+
+
+type BoxType
+    = Header
+    | Time
+
+
+createGrid : Element msg
+createGrid =
     Element.row
         []
-        (List.map
-            (\x -> createBox x)
-            daysOfWeek
-        )
+        (map (\x -> createDayColumn x) days)
 
 
+createDayColumn : String -> Element msg
+createDayColumn day =
+    let
+        dayBox =
+            [ createBox Header day ]
 
--- createRow : Element msg
--- createRow =
---     Element.row
---         []
---         (List.repeat 7 createBox)
+        timeBoxes =
+            map (\x -> createBox Time x) times
+    in
+    Element.column
+        []
+        (dayBox ++ timeBoxes)
 
 
-createBox : String -> Element msg
-createBox boxText =
-    Element.el
-        [ width (px 100)
-        , height (px 37)
-        , Background.color (rgb255 255 255 255)
-        , Border.color (rgb255 20 20 20)
-        , Border.width 1
-        , Font.color (rgb255 0 0 0)
-        , Font.center
-        , Font.size 16
-        ]
-        (text boxText)
+createBox : BoxType -> String -> Element msg
+createBox boxType boxText =
+    case boxType of
+        Header ->
+            Element.el
+                [ width (px 100)
+                , height (px 37)
+                , Background.color (rgb255 0 0 0)
+                , Border.color (rgb255 180 180 180)
+                , Border.width 1
+                , Font.color (rgb255 255 255 255)
+                , Font.center
+                , Font.size 16
+                ]
+                (text boxText)
+
+        Time ->
+            Element.el
+                [ width (px 100)
+                , height (px 37)
+                , Background.color (rgb255 255 255 255)
+                , Border.color (rgb255 180 180 180)
+                , Border.width 1
+                , Font.color (rgb255 0 0 0)
+                , Font.center
+                , Font.size 14
+                , Element.mouseOver
+                    [ Background.color (rgb255 60 208 112) ]
+                ]
+                (text boxText)
